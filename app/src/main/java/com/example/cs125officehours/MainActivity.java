c110ba949b5d6ee9c74a5810fa2925c8d50ede6f
Zayyan <zfaizal2@illinois.edu>
2019-04-24 11:30:16 -0500

    User Interface update

--- app/src/main/java/com/example/cs125officehours/MainActivity.java
+++ app/src/main/java/com/example/cs125officehours/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,45 +15,23 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
-    private Button button;
-    private Button button2;
-    private Button button3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button4);
-        //goes back to login from student
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openLogin();
-            }
-        });
-        //Opens student activity from course login
-        button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openStudentActivity();
-            }
-        });
-        button3 = (Button) findViewById(R.id.)
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
+
         myRef.setValue("CS");
     }
-    public void openLogin() {
+
+    public void onClick(View view) {
+
+    }
+    public void openLogin(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
-    }
-    public void openStudentActivity() {
-        Intent openStudent = new Intent(this, StudentActivity.class);
-        startActivity(openStudent);
-    }
-    public void openStaff(View view) {
-        Log.d(LOG_TAG,"Button Clicked");
-        Intent openStaff = new Intent(this, StaffActivity.class);
-        startActivity(openStaff);
     }
 }
